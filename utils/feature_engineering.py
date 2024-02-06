d2912e61a9e03a12103f28f03f64509e267b3e53
ok3ks <freelanceokeks@gmail.com>
2024-02-06 16:32:23 +0000

    Modularised code blocks into functions

--- utils/feature_engineering.py
+++ utils/feature_engineering.py
@@ -2,6 +2,7 @@ from typing import Union
 from pyspark.sql import SparkSession
 from pyspark.sql import Window
 from pyspark.sql import functions as F 
+from pyspark.ml import Pipeline
 
 def mult_60(mins):
     """
@@ -12,7 +13,7 @@ def mult_60(mins):
 
 def generate_window(window_in_minutes:int,
                     partition_by:str,
-                    timestamp_col:str):
+                    timestamp_col:str) -> Window:
     """This function generates a window, the column specified in orderBy
     is propagated to rangeBetween, Window can also work without orderBy"""
     window = (
@@ -21,15 +22,15 @@ def generate_window(window_in_minutes:int,
         .rangeBetween(-mult_60(window_in_minutes), -1))
     return window
 
-#difference between partition by and group by
-#- partition by adds column, group by aggregates column
-#- partition by examines in more detail with features like 
-#rows between, and range between
 def generate_rolling_aggregate(col:str,
                                 partition_by: Union[str,None] = None,
                                 operation: str = "count", 
                                 timestamp_col:str = "dt",
                                 window_in_minutes:int = 1,):
+    
+    """ This function generates a rolling aggregate given an 
+    operation which can be 'count', 'sum' or 'avg' """
+    
     if partition_by == None :
         partition_by = col
     
@@ -50,3 +51,56 @@ def generate_rolling_aggregate(col:str,
                             timestamp_col= timestamp_col))
     else:
         raise ValueError(f"Operation {operation} is not defined")
+    
+def numerical_features() -> list:
+    "Returns a list of numerical features"
+    
+    return ['duration', 'orig_bytes', 'resp_bytes',
+            'orig_pkts','orig_ip_bytes','resp_pkts',
+            'resp_ip_bytes', 'source_ip_count_last_min',
+            'source_ip_count_last_30_min']
+
+def categorical_features() -> list:
+    "Returns a list of categorical features"
+
+    return ["proto", "service", "conn_state"]
+
+def categorical_features_indexed(features = categorical_features()) -> list:
+    "Returns a list of categorical features indexed"
+
+    categorical_features_indexed = [c + "_index" for c in features]
+    return categorical_features_indexed
+
+def one_hot_encoded_index(features = categorical_features()) -> list:
+    "Returns one hot encoded index"
+    one_hot_encoded_index = [c + "_one_hot" for c in features]
+    return one_hot_encoded_index
+
+def input_features(*features:list) -> list:
+    " Returns all columns to be used as features"
+    
+    temp = []
+    for a in features:
+        temp.extend(a)
+    return temp
+
+def compute_rolling_aggregate() -> dict:
+    "Computes rolling aggregate of a minutes and last 30 minutes"
+
+    return {
+        "source_ip_avg_bytes_last_min": generate_rolling_aggregate(col="orig_ip_bytes", operation="avg",timestamp_col="dt",window_in_minutes=1),
+        "source_ip_avg_bytes_last_30_min": generate_rolling_aggregate(col="orig_ip_bytes", operation="avg",timestamp_col="dt",window_in_minutes=30),
+        "dest_ip_count_last_min": generate_rolling_aggregate(col="destination_ip", operation="count",timestamp_col="dt",window_in_minutes=1),
+        "dest_ip_count_last_30_min": generate_rolling_aggregate(col="destination_port", operation="count",timestamp_col="dt",window_in_minutes=30),
+        "dest_port_count_last_min": generate_rolling_aggregate(col="destination_ip", operation="count",timestamp_col="dt",window_in_minutes=1),
+        "dest_port_count_last_30_min": generate_rolling_aggregate(col="destination_port", operation="count",timestamp_col="dt",window_in_minutes=30),
+        "source_ip_avg_pkts_last_min": generate_rolling_aggregate(col="orig_pkts",partition_by="source_ip", operation="avg",timestamp_col="dt",window_in_minutes=1),
+        "source_ip_avg_pkts_last_30_min": generate_rolling_aggregate(col="orig_pkts",partition_by="source_ip",operation="avg",timestamp_col="dt",window_in_minutes=30)
+    }
+
+def get_feature_importance(pipeline:Pipeline) -> dict :
+
+    return {
+        "importance": list(pipeline.stages[-1].featureImportances),
+        "feature": pipeline.stages[-2].getInputCols(),
+    }
---
+++ utils/ml.py
@@ -0,0 +1,48 @@
+from pyspark.ml.feature import StringIndexer, VectorAssembler, OneHotEncoder
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.sql import SparkSession
+
+from typing import Union,Any
+from utils.feature_engineering import categorical_features, numerical_features, input_features
+from utils.feature_engineering import one_hot_encoded_index, categorical_features_indexed
+
+def get_Pipeline(categorical_features: Union[Any, list] = categorical_features(), 
+                 numerical_features: Union[Any, list] = numerical_features(),
+                 labelCol= "good") -> Pipeline :
+    """ 
+    Categorical features are passed through a StringIndexer and OneHotEncoder 
+    """
+    #spark = SparkSession.builder.appName("anomaly").getOrCreate()
+
+    if categorical_features :
+        indexer = StringIndexer(inputCols=categorical_features, outputCols=categorical_features_indexed(categorical_features), handleInvalid='skip')
+        one_hot_encoder = OneHotEncoder(inputCols=categorical_features_indexed(categorical_features), outputCols = one_hot_encoded_index(categorical_features), handleInvalid='skip')
+
+        inputCols = input_features(one_hot_encoded_index(categorical_features))
+
+    if numerical_features :
+        pass
+
+    if categorical_features and numerical_features:
+        inputCols = input_features(one_hot_encoded_index(categorical_features), numerical_features)
+
+    assembler = VectorAssembler(inputCols=inputCols, outputCol="features", handleInvalid="skip")
+    randomforest = RandomForestClassifier(featuresCol="features", labelCol = labelCol, numTrees=100)
+    pipeline = Pipeline(stages=[indexer, assembler,one_hot_encoder, randomforest])
+
+    return pipeline
+
+def get_evaluator(labelCol= "good", metricName = "areaUnderPR") -> BinaryClassificationEvaluator:
+    """
+    Returns a Binary Classification Evaluator.
+    
+    Specify labelCol according to your dataset, should be the column name of your target
+    Specify metricName according to PySpark Docs 
+    """
+    return BinaryClassificationEvaluator(labelCol=labelCol, metricName=metricName)
+    
+
+if __name__ == "__main__":
+    pass
